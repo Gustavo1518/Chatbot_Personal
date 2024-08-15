f805400ad27161726d74b9c012d6b8b136c1c629
Gustavo1518 <gmeneses137@gmail.com>
2024-08-15 14:52:34 -0600

    se corrige bug de inactividad

--- flows/flowprincipal.js
+++ flows/flowprincipal.js
@@ -4,11 +4,13 @@ const VALIDACIONES = require('./validaciones.js');
 const flowHabilidades = require('./habilidades.js')
 const flowCotizar = require('./cotizar.js');
 
+const flowFinal = addKeyword(EVENTS.ACTION).addAnswer('👋 ¡Hasta luego!')
+
 const flowPrincipal = addKeyword(['hola', 'menu', 'menú'])
     .addAnswer('🙌 ¡Hola! Soy *Gustavo Meneses*, un desarrollador RPA especializado en Automation Anywhere, Python y Selenium. ¿Te gustaría conocer más sobre mi experiencia?')
-    .addAnswer(' *¿En que puedo ayudarte hoy?*\n\n1️⃣ Experiencia Profesional.\n2️⃣ Habilidades Técnicas\n3️⃣ Quiero una automatización', {capture: true, delay: 1000, idle: 180000}, (ctx, {fallBack,endFlow}) => {
+    .addAnswer(' *¿En que puedo ayudarte hoy?*\n\n1️⃣ Experiencia Profesional.\n2️⃣ Habilidades Técnicas\n3️⃣ Quiero una automatización', {capture: true, delay: 1000, idle: 180000}, (ctx, {fallBack,gotoFlow}) => {
             if(ctx?.idleFallBack) return endFlow('¡Hasta luego! 👋')
-            if(!VALIDACIONES.opcionValida(ctx.body)) return fallBack('Ingresa una opcion del menú')
+            if(!VALIDACIONES.opcionValida(ctx.body)) return gotoFlow(flowFinal)
         },[flowExperiencia,flowHabilidades, flowCotizar]
     );
 
